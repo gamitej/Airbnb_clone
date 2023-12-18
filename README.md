15e07339d05a858a908259b0412175357bdbf8b7
gamitej <gamitejpratapsingh998@gmail.com>
2023-12-18 10:45:05 +0530

    START : airbnb clone

---
+++ README.md
@@ -0,0 +1,1 @@
+## Airbnb Clone NodeJs Server
---
+++ server/app.js
@@ -0,0 +1,50 @@
+/**
+ * Project Name: Airbnb Clone
+ * Description: Airbnb is an online marketplace that connects people who want to rent out their property with people who are looking for accommodations, typically for short stays.
+ *
+ * Author: Amitej Pratap Singh
+ * Email: amitej.singh26@gmail.com
+ */
+
+require("dotenv").config();
+const express = require("express");
+const Database = require("./config/database.js");
+// libs
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// database connection
+const db = new Database(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+db.connect().catch((err) =>
+  console.error("Error connecting to database:", err)
+);
+
+// middlewares
+app.use(cors());
+app.use(morgan("common"));
+
+// server status & port number
+app.get("/server-status", (req, res) => {
+  res.status(200).json({ message: "Server is up and running!" });
+});
+
+// close the connection
+process.on("SIGINT", async () => {
+  try {
+    await db.disconnect();
+    console.log("Disconnected from database.");
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}!`));
